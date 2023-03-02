4c30d8e69b6f6c9f0e6e52376d3b4795a3acd5ff
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2023-03-02 20:50:42 -0300

    aula 2-3

---
+++ exemplos/004-atividade-if.js
@@ -0,0 +1,5 @@
+// Crie um programa que receba duas notas de 0 a 10, 
+// calcule a média e com a média
+// exiba as seguintes mensagens para cada faixa de valores
+// nota < 5 = I, nota >= 5 e < 6.5 = R, nota > 6.5 e < 8.5 = B 
+// e nota >= 8.5 MB
